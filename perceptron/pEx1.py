fc214aa6f91f54c67d5a652a6067ae265f645722
e4e6 <hangyeol460@gmail.com>
2024-01-09 13:26:43 +0900

    오류 수정

--- perceptron/pEx1.py
+++ perceptron/pEx1.py
@@ -20,13 +20,14 @@ def OR(x1,x2):
         return 0
     elif tmp > theta:
         return 1
-def XOR(x1,x2):
-    w1, w2, theta = -0.7, -0.7, -0.5
-    tmp = x1*w1 + x2*w2
-    if tmp <= theta:
-        return 0
-    elif tmp > theta:
-        return 1
+# def XOR(x1,x2):
+#     w1, w2, theta = -0.7, -0.7, -0.5
+#     tmp = x1*w1 + x2*w2
+#     if tmp <= theta:
+#         return 0
+#     elif tmp > theta:
+#         return 1
+# XOR 게이트 구현 불가 (x1*w1 + x2*w2 -theta(+bias) 그래프로 나타내면 1차함수 직선 형태 -> 곡선 형태 (비선형)으로는 구현 가능)
 
 print("(0,0), (0,1), (1,1)")
 print("AND",AND(0,0), AND(0,1), AND(1,1))
